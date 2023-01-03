5233e40c5d55bab43d340e2e365fb1d59fd59126
Alejandro Cerezo <alce65@hotmail.es>
2023-01-03 11:08:01 +0100

    Add emoji files

---
+++ src/CH1.GoT/services/emojis.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* export const emoji = {
+    rey: '👑',
+    luchador: '🗡',
+    asesor: '🎓',
+    escudero: '🛡',
+}; */
+
+export enum emoji {
+  'rey' = '👑',
+  'luchador' = '🗡',
+  'asesor' = '🎓',
+  'escudero' = '🛡',
+}
