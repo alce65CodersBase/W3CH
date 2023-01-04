9b1952bae48a91a725edf68885e556ed0242b42c
Alejandro Cerezo <alce65@hotmail.es>
2023-01-04 12:48:59 +0100

    Add try/catch block: solve bug

--- src/CH2.GoT/main_got2.ts
+++ src/CH2.GoT/main_got2.ts
@@ -1,4 +1,10 @@
 import { App } from './components/app/app';
+import { consoleDebug } from './tools/debug';
 
 console.log('Loaded App');
-new App();
+
+try {
+  new App();
+} catch (error) {
+  consoleDebug((error as Error).message);
+}
