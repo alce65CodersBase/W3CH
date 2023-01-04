491bca44c301a2a46638afe0092c991b16e3fd12
Alejandro Cerezo <alce65@hotmail.es>
2023-01-04 08:43:08 +0100

    Add new page in Vite project

---
+++ src/CH2.GoT/main_got2.ts
@@ -0,0 +1,6 @@
+//For later import { appGoT } from './app';
+import { createCharacters } from '../CH1.GoT/services/characters';
+
+const characters = createCharacters();
+console.log('Loaded', characters);
+//For later appGoT(characters);
