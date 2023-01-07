5e8d31c2bb430243a0384db8411371e18557c321
Alejandro Cerezo <alce65@hotmail.es>
2023-01-07 17:10:36 +0100

    Update serias page for filter series by state

--- src/CH4.Series/pages/series/series.ts
+++ src/CH4.Series/pages/series/series.ts
@@ -1,27 +1,48 @@
 import { Component } from '../../../lib/component/component';
+import { consoleDebug } from '../../../lib/tools/debug';
 import { Header } from '../../components/header/header';
 import { List } from '../../components/list/list';
+import { Series } from '../../models/series';
+import { getSeries } from '../../services/mock.repo';
 import series__ from './series.module.css';
 
 export class SeriesPage extends Component {
   children: Array<Component>;
+  series: Array<Series>;
   constructor(private selector: string) {
     super();
     this.template = this.createTemplate();
     this.children = [];
-    this.render();
+    this.series = [];
+    this.loadSeries();
   }
 
   render() {
     const element = super.innRender(this.selector);
     const child = new Header('.header-slot');
     this.children.push(child);
-    const child2 = new List('section.list-slot');
+    const pendingSeries = this.series.filter((item) => item.score === 0);
+    const child2 = new List(
+      'section.list-slot',
+      'series-pending',
+      pendingSeries
+    );
     this.children.push(child2);
     // Segunda lista para series vistas
-    const child3 = new List('section.list-slot', 'series-watched');
+    const watchedSeries = this.series.filter((item) => item.score > 0);
+    const child3 = new List(
+      'section.list-slot',
+      'series-watched',
+      watchedSeries
+    );
     this.children.push(child3);
     return element;
+  }
+
+  loadSeries() {
+    this.series = getSeries();
+    consoleDebug(this.series);
+    this.render();
   }
 
   private createTemplate() {
